c830a76eca8f8176e5f2987d42d674c2e3520c47
Gabriel Majeri <gabriel.majeri6@gmail.com>
2019-09-23 19:59:14 +0300

    Implement doubleEveryOther function

--- src/Homework1.hs
+++ src/Homework1.hs
@@ -1,5 +1,7 @@
 module Homework1 where
 
+import Debug.Trace
+
 toDigits :: Integer -> [Integer]
 toDigits n = reverse $ toDigitsRev n
 
@@ -7,3 +9,10 @@ toDigitsRev :: Integer -> [Integer]
 toDigitsRev n
     | n <= 0 = []
     | otherwise = (n `mod` 10) : toDigitsRev (n `div` 10)
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther arr = reverse $ doubleFromStart $ reverse arr
+    where
+        doubleFromStart [] = []
+        doubleFromStart (x:[]) = [x]
+        doubleFromStart (x:y:xs) = [x, y * 2] ++ (doubleFromStart xs)
--- test/Spec.hs
+++ test/Spec.hs
@@ -15,3 +15,8 @@ main = hspec $ do
         describe "toDigitsRev" $ do
             it "converts a positibe integer to the reverse array of its digits" $
                 HW1.toDigitsRev 1234 `shouldBe` [4, 3, 2, 1]
+        describe "doubleEveryOther" $ do
+            it "doubles every other number from the right" $
+                HW1.doubleEveryOther [8, 7, 6, 5] `shouldBe` [16, 7, 12, 5]
+            it "works with odd length arrays" $
+                HW1.doubleEveryOther [1, 2, 3] `shouldBe` [1, 4, 3]
